9df82e2a68179f7b44ac0c93cad6ed2fd2c4a52f
Dennis Dashevsky <d3nsin@gmail.com>
2023-02-27 21:14:21 +0200

    Create 25. Exiting Loops.cpp

---
+++ 4.Control Flow/25. Exiting Loops.cpp
@@ -0,0 +1,32 @@
+/*Goal: understand the break and conitnue statements*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    while(a < 5)
+    {
+        cout<<"a = "<<a<<"\n";       
+        a++;
+        if(a == 3)
+        break;
+    }
+    cout<<"The first statement after the first while loop\n\n";
+    
+    
+    while(a < 15)
+    {
+        a++;
+        if(a == 10)
+        {
+            cout<<"\tWhen a=10, go back to the top of the loop";
+            cout<<"\n\tThis means a=10 is skipped.\n";
+            continue;
+        }
+        std::cout<<"After continue a = "<<a<<"\n";           
+    }
+    return 0;
+}
