3a9e371f2468d4550b48a3b1232be3a190f4f397
Dennis Dashevsky <d3nsin@gmail.com>
2023-03-02 08:34:58 +0200

    Create 09. Function w Return Values.cpp

---
+++ 7.Functions/09. Function w Return Values.cpp
@@ -0,0 +1,43 @@
+/*Goal: practice returning values from functions*/
+
+#include<iostream>
+
+using namespace std;
+
+float add(float m1, float m2);
+float sub(float m1, float m2);
+float mult(float m1, float m2);
+float div(float m1, float m2);
+
+int main()
+{
+    float m1 = 4.0;
+    float m2 = 3.5;
+
+
+    cout<<m1<<"+"<<m2<<" = "<<add(m1, m2)<<"\n";
+    cout<<m1<<"-"<<m2<<" = "<<sub(m1,m2)<<"\n";
+    cout<<m1<<"*"<<m2<<" = "<<mult(m1,m2)<<"\n";
+    cout<<m1<<"/"<<m2<<" = "<<div(m1,m2)<<"\n";
+    return 0;
+}
+
+float add(float m1, float m2)
+{
+    return m1 + m2;
+}
+
+float sub(float m1, float m2)
+{
+    return m1 - m2;
+}
+
+float mult(float m1, float m2)
+{
+    return m1 * m2;
+}
+
+float div(float m1, float m2)
+{
+    return m1 / m2;
+}
