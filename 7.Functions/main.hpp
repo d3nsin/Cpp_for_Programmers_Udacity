be74193e6b9e1768cd115be27b3c9ef78d3f732e
Dennis Dashevsky <d3nsin@gmail.com>
2023-03-02 08:11:47 +0200

    Create 07. Function in Header Files,cpp and its main.hpp

---
+++ 7.Functions/main.hpp
@@ -0,0 +1,11 @@
+/*The header file for main.cpp*/
+
+void printProduct(int m1, int m2, int product);
+
+using namespace std;
+
+
+void printProduct(int m1, int m2, int product)
+{
+    cout<<m1<<" * "<<m2<<" = "<<product;
+}
