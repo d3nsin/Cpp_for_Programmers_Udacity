c7366e248ce545397d4fe04691f21a003a1dd2c0
Dennis Dashevsky <d3nsin@gmail.com>
2023-03-02 13:02:56 +0200

    Create 10. Function w Ret. Values Programming,cpp and main2.hpp

---
+++ 7.Functions/10. Function w Ret. Values Programming.cpp
@@ -0,0 +1,13 @@
+/*Goal: practice writing functions with parameters and return values.*/
+
+#include"main2.hpp"
+
+int main()
+{
+    int m1=10;
+    int m2=20;
+    
+    printEquation(m1, m2, sum(m1, m2), '+');
+    return 0;
+    
+}
