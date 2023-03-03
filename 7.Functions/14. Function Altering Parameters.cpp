ca3f555e8473ea44550b781374e76c2aaedc8326
Dennis Dashevsky <d3nsin@gmail.com>
2023-03-03 11:29:07 +0200

    Update 14. Function Altering Parameters.cpp

--- 7.Functions/14. Function Altering Parameters.cpp
+++ 7.Functions/14. Function Altering Parameters.cpp
@@ -4,18 +4,20 @@
 
 #include<iostream>
 
+using namespace std;
+
 int increment(int input);
 int main()
 {
     int a = 34;
-    std::cout<<"Before the function call a = "<<a<<"\n";
+    cout<<"Before the function call a = "<<a<<"\n";
     a = increment(a);
-    std::cout<<"After the function call a = "<<a<<"\n";
+    cout<<"After the function call a = "<<a<<"\n";
     return 0;
 }
 int increment(int input)
 {
     input++;
-    std::cout<<"In the function call a = "<<input<<"\n";
+    cout<<"In the function call a = "<<input<<"\n";
     return input;
 }
