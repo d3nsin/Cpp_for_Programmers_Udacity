12b541f2d363e4cd83f92d6b3dfd27f449d65fab
Dennis Dashevsky <d3nsin@gmail.com>
2023-03-03 11:29:10 +0200

    Update 15. Pass Variable by Reference.cpp

--- 7.Functions/15. Pass Variable by Reference.cpp
+++ 7.Functions/15. Pass Variable by Reference.cpp
@@ -6,18 +6,20 @@
 
 #include<iostream>
 
+using namespace std;
+
 void increment(int &input); //Note the addition of '&'
 
 int main()
 {
     int a = 34;
-    std::cout<<"Before the function call a = "<<a<<"\n";
+    cout<<"Before the function call a = "<<a<<"\n";
     increment(a);
-    std::cout<<"After the function call a = "<<a<<"\n";
+    cout<<"After the function call a = "<<a<<"\n";
     return 0;
 }
 void increment(int &input)//Note the addition of '&'
 {
     input++; //**Note the LACK OF THE addition of '&'**
-    std::cout<<"In the function call a = "<<input<<"\n";
+    cout<<"In the function call a = "<<input<<"\n";
 }
