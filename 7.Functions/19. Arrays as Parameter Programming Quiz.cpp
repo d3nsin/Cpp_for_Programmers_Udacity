912ac3cfbe8995c29b101d5d7a7186b1c8b55f0c
Dennis Dashevsky <d3nsin@gmail.com>
2023-03-05 21:07:54 +0200

    Create 19. Arrays as Parameter Programming Quiz.cpp

---
+++ 7.Functions/19. Arrays as Parameter Programming Quiz.cpp
@@ -0,0 +1,34 @@
+/*Goal: create a function that searches an array for 
+**a given value. 
+*/
+
+#include<iostream>
+using namespace std;
+
+int search(int *array, int size, int searchKey);
+
+int main()
+{
+    const int size = 4;
+    int array[] = {345,75896,2,543};
+    int searchKey = 543;
+    cout<<"Found at: "<<search(array, size, searchKey);
+    return 0;
+}
+
+int search(int *array, int size, int searchKey)
+{
+    int location;
+    for(int i=0; i<size; i++)
+    {
+        if (array[i] == searchKey)
+        {
+            location = i;
+        }
+        else
+        {
+            location = -1;
+        }
+    }
+    return location;
+}
